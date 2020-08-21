37e0e128ec12a0636fa16d9f15e4d25566c39e3b
engerb <engerbewza@gmail.com>
2020-08-21 14:08:51 -0700

    Texture loading with promises

--- src/components/Store.js
+++ src/components/Store.js
@@ -5,32 +5,36 @@ import { sRGBEncoding, TextureLoader, convertSRGBToLinear } from "three";
 
 // create for setting colours too
 
-const loadTexture = (url) => {
-    return new TextureLoader().load( url,
-        ( texture ) => {
+// Can return the texture object or a promise wrap for it so that the state is only changed when ready
+const loadTexture = (url, promise = true) => {
+    if (promise) {
+        return new Promise(resolve => {
+            new TextureLoader().load(url, ( texture ) => {
+                texture.flipY = false;
+                texture.encoding = sRGBEncoding;
+                resolve(texture);
+            });
+        });
+    } else {
+        return new TextureLoader().load(url, ( texture ) => {
             texture.flipY = false;
             texture.encoding = sRGBEncoding;
-        },
-        undefined,
-        ( err ) => {
-            console.error( 'Loading lid wrap: An error happened.' );
-        }
-    );
+        });
+    }
 }
 
 const [useStore, api] = create(set => ({
     hdri: require('../assets/3d/venice_sunset_1k.hdr').default,
-    binDecalUrl: loadTexture(DefaultCMF.binDecals[ DefaultCMF.binDecals.findIndex(obj => obj.selected) ].img),
-    lidDecalUrl: loadTexture(DefaultCMF.lidDecals[ DefaultCMF.lidDecals.findIndex(obj => obj.selected) ].img),
+    binDecalUrl: loadTexture(DefaultCMF.binDecals[ DefaultCMF.binDecals.findIndex(obj => obj.selected) ].img, false),
+    lidDecalUrl: loadTexture(DefaultCMF.lidDecals[ DefaultCMF.lidDecals.findIndex(obj => obj.selected) ].img, false),
     frontDecalUrl: null,
     rearTopDecalUrl: null,
     rearBottomDecalUrl: null,
     binColor: DefaultCMF.colors[ DefaultCMF.colors.findIndex(obj => obj.selectedBin) ].color,
     lidColor: DefaultCMF.colors[ DefaultCMF.colors.findIndex(obj => obj.selectedLid) ].color,
 
-    // Create promise and set on resolve! This will also trigger a re-render!
-    testMethodBin: () => set({binDecalUrl: loadTexture( DefaultCMF.binDecals[ 2 ].img )}),
-    testMethodLid: () => set({lidDecalUrl: loadTexture( DefaultCMF.lidDecals[ 2 ].img )})
+    testMethodBin: () => loadTexture( DefaultCMF.binDecals[ 2 ].img ).then((result) => { set({binDecalUrl: result}) }),
+    testMethodLid: () => loadTexture( DefaultCMF.lidDecals[ 2 ].img ).then((result) => { set({lidDecalUrl: result}) })
     // methods
     // stuff to store and add to DefaultCMF
 }));
