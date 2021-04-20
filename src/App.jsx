7a6ac9fcc7ddb8f4949132a80b8a65ef7647d661
Enger Bewza <engerbewza@gmail.com>
2021-04-19 23:53:00 -0700

    adding back in

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+// import useStore from './store/Store'
+import logo from './logo.svg';
+
+// eslint-disable-next-line
+export default () => {
+  const storeProps = {
+    // loaded: useStore(state => state.loaded),
+    // error: useStore(state => state.error),
+  }
+
+  return (
+    <div>
+      HELLO!
+    </div>
+  )
+}
